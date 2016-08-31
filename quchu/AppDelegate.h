c0dab6609ea469635cd4ff95d15148dd7ac84baf
huangbinmin <huangbinmin@huangbinmindemacbook-air.local>
2016-08-31 17:10:50 +0800

    Initial Commit

---
+++ quchu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  quchu
+//
+//  Created by 黄斌民 on 16/8/31.
+//  Copyright © 2016年 Bin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
